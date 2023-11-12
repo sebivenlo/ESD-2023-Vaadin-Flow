faaee874ee7076e4c22ce632497acd80f9614999
Fabian Kerzmann <453734@student.fontys.nl>
2023-11-12 15:58:33 +0100

    :wastebasket: Removes solutions folder
    Solutions of task 3 are now moved into vaadin-demo folder

--- solutions/task3/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.esde.views.task3;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
